35f46a3145c85aa7c4d335b15311e8382d044475
RomanovIgnat <65200927+RomanovIgnat@users.noreply.github.com>
2020-05-13 20:24:47 +0300

    Update time_logger_decorator.py

--- time_logger_decorator.py
+++ time_logger_decorator.py
@@ -5,6 +5,17 @@ import time
 
 
 def time_logger(timeout=None, time_format="%c", cash=False):
+    
+    """
+    Allows calling decorator which measures function's execution time
+
+    :param timeout: if decorated function had not finished working until timeout ended, the decorator would raise
+                    an exception
+                    Alert if function works forever, thread will flow
+    :param time_format: time format
+    :param cash: if cash = True, input arguments will be saved
+    """
+    
     def time_decorator(func_to_decorate):
 
         @functools.wraps(func_to_decorate)
